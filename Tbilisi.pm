9007dd24b7a0c4720cceca1e631bb152c4c76cba
Michal Špaček <tupinek@gmail.com>
2015-01-03 11:58:21 +0100

    Prepare for 0.03 version.

--- Tbilisi.pm
+++ Tbilisi.pm
@@ -11,7 +11,7 @@ use Moo;
 use namespace::clean;
 
 # Version.
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Get XML.
 has xml => (
@@ -241,6 +241,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
