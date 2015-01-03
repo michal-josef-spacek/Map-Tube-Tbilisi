c510f5a08686a6dc39fae811bc3ed907d9a231c3
Michal Špaček <tupinek@gmail.com>
2015-01-03 11:57:27 +0100

    Add EXAMPLE3 section to doc.

--- Tbilisi.pm
+++ Tbilisi.pm
@@ -147,6 +147,44 @@ For more information about Tbilisi Map, click L<here|https://en.wikipedia.org/wi
  # Output like:
  # XML file: .*/tbilisi-map.xml
 
+=head1 EXAMPLE3
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Map::Tube::GraphViz;
+ use Map::Tube::GraphViz::Utils qw(node_color_without_label);
+ use Map::Tube::Tbilisi;
+
+ # Object.
+ my $obj = Map::Tube::Tbilisi->new;
+
+ # GraphViz object.
+ my $g = Map::Tube::GraphViz->new(
+         'callback_node' => \&node_color_without_label,
+         'driver' => 'neato',
+         'tube' => $obj,
+ );
+
+ # Get graph to file.
+ $g->graph('Tbilisi.png');
+
+ # Print file.
+ system "ls -l Tbilisi.png";
+
+ # Output like:
+ # -rw-r--r-- 1 skim skim 68209 Jan  3 11:56 Tbilisi.png
+
+=begin html
+
+<a href="https://raw.githubusercontent.com/tupinek/Map-Tube-Tbilisi/master/images/ex3.png">
+  <img src="https://raw.githubusercontent.com/tupinek/Map-Tube-Tbilisi/master/images/ex3.png" alt="Pražské metro" width="300px" height="300px" />
+</a>
+
+=end html
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
